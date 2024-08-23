6fcef2228eb09d9ffef1a88cc04c4dbab2bcccc2
Jake <jacobj@spacelift.io>
2024-08-23 10:38:32 -0700

    update provider config

--- spacelift-admin/providers.tf
+++ spacelift-admin/providers.tf
@@ -2,7 +2,15 @@ terraform {
   required_providers {
     spacelift = {
       source  = "spacelift-io/spacelift"
-      version = "1.15.0"
+      version = "1.14.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
